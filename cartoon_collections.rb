dc8754446f8c9b3aba8579ac8f8ed23762f57d5b
Jallen Messersmith <jallenmessersmith@gmail.com>
2019-08-19 21:36:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.find do |array|
+  array.each do |array|
     if cheese_types.include?(array)
       return  
     else 
